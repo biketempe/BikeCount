e100015c5744ae9a3f05d5e4f0e99b5246666d8b
Scott Walters <scott@slowass.net>
2014-02-26 12:42:58 -0600

    geocoding support for Google's API; fix location duplication detection, which is used as error detection to catch when geolocation is failing and the geocoder doesn't tell us.

--- geo.pm
+++ geo.pm
@@ -4,8 +4,9 @@ package geo;
 use strict;
 use warnings;
 
-# use Geo::Coder::RandMcnally; # overlaps most of the intersections; sucks
-# use Geo::Coder::Geocoder::US;  # seems to do a pretty good job but can't handle the canals and some other things; most stuff was geocoded with this one
+use Geo::Coder::RandMcnally; # overlaps most of the intersections; sucks
+use Geo::Coder::Geocoder::US;  # seems to do a pretty good job but can't handle the canals and some other things; most stuff was geocoded with this one
+use Geo::Coder::Google; # can't do intersections with a freeway to save its life and doesn't know when it has failed; fucks up a lot of small streets too; actually may be failing on everything
 use Geo::Coder::TomTom; 
 
 sub geocode {
@@ -31,7 +32,7 @@ sub geocode {
 
     my %locs;
     for my $row ( $count_sites->rows ) {
-        next if $row->longitude and $row->latitude;
+        next unless $row->longitude and $row->latitude;
         my $location_desc = $row->location_N_S . ' & ' . $row->location_W_E . ', Tempe, AZ';
         $locs{ $row->longitude . '_' . $row->latitude } = $location_desc;
     }
@@ -41,22 +42,28 @@ sub geocode {
         next if $row->longitude and $row->latitude;
         # next if $row->automatic_geocoding_failed; # XXX
 
+        next unless $row->location_N_S or $row->location_W_E;
+        warn "``" . $row->location_N_S . "'' ``" . $row->location_W_E . "''";
+        # my $location_desc = $row->location_N_S . ' & ' . $row->location_W_E . ', Tempe, AZ';
+        my $location_desc = ( $row->location_N_S and $row->location_W_E ) ? $row->location_N_S . ' & ' . $row->location_W_E : $row->location_N_S . $row->location_W_E;
+        $location_desc .= ', Tempe, AZ'; # XXX hard-code
+
         # my $geocoder = Geo::Coder::RandMcnally->new;
         # my $geocoder = Geo::Coder::Geocoder::US->new;
-        # my $geocoder = Geo::Coder::Geocoder::US->new;
+        # my $geocoder = Geo::Coder::Google->new;
         my $geocoder = Geo::Coder::TomTom->new;
-
-        my $location_desc = $row->location_N_S . ' & ' . $row->location_W_E . ', Tempe, AZ';
 
         my $location = $geocoder->geocode(
             location => $location_desc,
         );
 
         if( $location and ! $location->{error} ) {
-            $row->longitude = $location->{lon} || $location->{long} || $location->{longitude} or die Data::Dumper::Dumper $location;
-            $row->latitude = $location->{lat} || $location->{latitude} or die Data::Dumper::Dumper $location;
+            $row->longitude = $location->{lon} || $location->{long} || $location->{longitude} || $location->{geometry}->{location}->{lng} or die Data::Dumper::Dumper $location;
+            $row->latitude = $location->{lat} || $location->{latitude} || $location->{geometry}->{location}->{lat} or die Data::Dumper::Dumper $location;
+  
             $row->automatic_geocoding_failed = 0;
             warn "long/lat = " . $row->longitude . ', ' . $row->latitude;
+            $locs{ $row->latitude . '_' . $row->longtide } = $location_desc;
             if( my $previous = $locs{ $row->longitude . '_' . $row->latitude } ) {
                warn "duplicate lon/lat of previous ``$previous'' at ``$location_desc''";
                $row->longitude = '';
@@ -64,7 +71,6 @@ sub geocode {
                $row->automatic_geocoding_failed = 1;
                next;
             }
-            $locs{ $location->{lat} . '_' . $location->{lon} } = $location_desc;
             $row->geocoded_by = ref $geocoder;
         } else {
             warn $location->{error} if $location->{error};
