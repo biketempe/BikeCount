4445214eac1a956550fe9cfe68273000c32ca45f
Scott Walters <scott@slowass.net>
2015-02-28 12:46:24 -0700

    actually report on people who gave us good data
    fixed up sketchy.pl to actually use the correct sense of the test
    looks like I reversed it previously to do the opposite -- find
    people who *didn't* provide good data last year, which is a report
    which has since been folded into admin.pl/admin.cgi as the
    "Sketchy Assignments/Thursday Sketchy Assignments" reports.
    so now that that's in there, I can change it back to telling us
    who *should* be signing up for important stuff.  that means I
    now need to rename it, since the temporary reversal was actually
    a temporary reversal of a temporary reversal.

--- sketchy.pl
+++ sketchy.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/perl
 
-# just report on email addresses of users who generated good data; these volunteers are invited to register early for the high priority intersections
+# report on email addresses of users who generated good data
+# these volunteers are invited to register early for the high priority intersections
+
+# the output of this can be imported into the Mailchimp list of count Alumni at https://us4.admin.mailchimp.com/lists/members/?id=181049
 
 use strict;
 use warnings;
@@ -9,43 +12,32 @@ use csv;
 use Data::Dumper;
 use String::Approx;
 
-use lib 'continuity/lib'; # dev version
-use Continuity;
-use Continuity::Adapt::PSGI;
-
 use Data::Dumper;
 use IO::Handle;
 use Carp;  
-use Cwd;
 
 use csv;
-use geo;
 
-
-my $previous = csv->new('count_data_2013_post_cliff_fixes_extra_data_prune.csv', 0);
+my $previous = csv->new('2014_post_turk_post_volunteer_entry_checking_post_scotts_sanity_checks_post_cliff_conversion.csv', 0);  # this is the output of the Mechanical Turk process
+# my $previous = csv->new('count_data_2013_post_cliff_fixes_extra_data_prune.csv', 0);
 # my $previous = csv->new('2011_2012_combined.csv', 0);
 
-my $volunteers = csv->new('volunteers.csv', 0);
-
-#
-# find sketchy assignments where a new counter (or one who turned in no data in previous years) is on a high value intersection
-#
+my $volunteers = csv->new('volunteers-last-cp.csv', 0);
 
 my %prev_volunteer_names = map { ( $_->Recorder => 1 ) } $previous->rows;
 
 my @sketchy_assignments;
 
 for my $volunteer ( $volunteers->rows ) {
-    # first_name,last_name,phone_number,email_address,training_session,training_session_comment,intersections,comments
-    my $sketchy;
-    my $name = join ' ', map $volunteer->{$_}, qw/first_name last_name/;
-    if( grep length $_, map $volunteer->{$_}, qw/first_name last_name/ ) {
+    my @name_parts = grep { defined $_ and length $_ } map $volunteer->{$_}, qw/first_name last_name/;
+    if( 2 == @name_parts and $volunteer->email_address ) {
+        my $name = join ' ', @name_parts;
         my @matches = String::Approx::amatch($name, keys %prev_volunteer_names);
         if( @matches ) {
-            print $volunteer->email_address, "\n";
+            # print "$name matches @matches: email: ", $volunteer->email_address, "\n"; # debug
+            # print "$name <" . $volunteer->email_address . ">\n"; # Mailchimp considers this to be a "syntax error" but GMail wants 822 style email addresses except *without* commas seperating them (FFS, get it together people, this is why we invented RFCs)
+            print $volunteer->email_address, "\n";   # this is what Mailchimp wants for import
         }
     }
 }
 
-
-# test
